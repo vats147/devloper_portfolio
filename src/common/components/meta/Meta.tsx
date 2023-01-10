879e1e276de080ebdd11ccdede658e9dfee03ea2
vats147 <vatsalmangukiya9003@gmail.com>
2023-01-10 11:34:54 +0530

    Replace Name and URl

--- src/common/components/meta/Meta.tsx
+++ src/common/components/meta/Meta.tsx
@@ -2,8 +2,8 @@ const Meta = () => {
   return (
     <>
       <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
-      <link rel="canonical" href="https://www.raihanhosen.com/"></link>
-      <link rel="pingback" href="https://www.raihanhosen.com/"></link>
+      <link rel="canonical" href="https://devloper-portfolio.vercel.app"></link>
+      <link rel="pingback" href="https://devloper-portfolio.vercel.app"></link>
 
       <title> Vatsal Mangukiya: Portfolio </title>
 
@@ -17,17 +17,17 @@ const Meta = () => {
       <meta property="og:site_name" content="Vatsal Mangukiya" />
       <meta property="og:title" content='Vatsal Mangukiya | Portfolio' />
       <meta property="og:description" content='Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development India, Web Development India, Company, Services, India, Flexsin, Flexsin Technologies' />
-      <meta property="og:url" content='https://raihanhosen.com/' />
-      <meta property="og:image" content='https://raihanhosen.com/images/model/Vatsal_Mangukiya.jfif' />
+      <meta property="og:url" content='https://devloper-portfolio.vercel.app' />
+      <meta property="og:image" content='https://devloper-portfolio.vercel.app/images/model/Vatsal_Mangukiya.jfif' />
       <meta property="og:type" content="website" />
 
       
 
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@raihanhosen01" />
-      <meta name="twitter:creator" content="@raihanhosen01" />
+      <meta name="twitter:site" content="@VatsalMangukiy3" />
+      <meta name="twitter:creator" content="@VatsalMangukiy3" />
       <meta name="twitter:description" content='Highly motivated and very passionate Full Stack Developer with three years of experience in Typescript, React js, Next js, Firebase, Tailwind CSS, Material UI, Node js, Express js, MongoDB' />
-      <meta name="twitter:image" content='https://raihanhosen01.com/images/raihanhosen.png' />
+      <meta name="twitter:image" content='https://devloper-portfolio.vercel.app/images/model/Vatsal_Mangukiya.jfif'  />
     </>
   );
 };
